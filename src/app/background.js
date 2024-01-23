5a5650cf367a4ed9c210cef0f03e48641fce6637
J4meZ <58501310+J4mez@users.noreply.github.com>
2024-01-23 15:08:33 +0100

    added support for chrome

--- src/app/background.js
+++ src/app/background.js
@@ -1,19 +1,15 @@
 console.log("background.js");
+//Include the polyfill to support chrome
+import "/app/browser-polyfill.js";
 
 // Callback reads runtime.lastError to prevent an unchecked error from being
 // logged when the extension attempt to register the already-registered menu
 // again. Menu registrations in event pages persist across extension restarts.
-browser.contextMenus.create(
-    {
-        id: "search-with-google-lense",
-        title: "Search with google lense",
-        contexts: ["image"],
-    },
-
-    // See https://extensionworkshop.com/documentation/develop/manifest-v3-migration-guide/#event-pages-and-backward-compatibility
-    // for information on the purpose of this error capture.
-    () => void browser.runtime.lastError
-);
+browser.contextMenus.create({
+    id: "search-with-google-lens",
+    title: "Search with Google Lens",
+    contexts: ["image"],
+});
 
 // debuger
 /* browser.contextMenus.create(
@@ -21,29 +17,24 @@ browser.contextMenus.create(
         id: "check-type",
         title: "Debug: Check type",
         contexts: ["all"],
-    },
-
-    // See https://extensionworkshop.com/documentation/develop/manifest-v3-migration-guide/#event-pages-and-backward-compatibility
-    // for information on the purpose of this error capture.
-    () => void browser.runtime.lastError
-);
+    });
  */
 browser.contextMenus.onClicked.addListener((info, tab) => {
-    if (info.menuItemId === "search-with-google-lense") {
+    if (info.menuItemId === "search-with-google-lens") {
         var searchUrl = `https://lens.google.com/uploadbyurl?url=${info.srcUrl}`;
         console.log(searchUrl);
 
         // if the searchURL is SVG, display an error
         if (info.srcUrl.endsWith(".svg")) {
-            alert("SVG images are not supported by Google Lense");
+            alert("SVG images are not supported by Google Lens");
             notification(
                 "Error!",
-                "SVG images are not supported by Google Lense"
+                "SVG images are not supported by Google Lens"
             );
             return;
         }
 
-        // open new tab with google lense
+        // open new tab with google lens
         browser.tabs.create({
             url: searchUrl,
         });
@@ -58,7 +49,7 @@ browser.contextMenus.onClicked.addListener((info, tab) => {
 function notification(title, message) {
     browser.notifications.create("error-noti", {
         type: "basic",
-        iconUrl: browser.runtime.getURL("icons/icon96.png"),
+        iconUrl: runtime.getURL("icons/icon96.png"),
         title: title,
         message: message,
     });
